3f2cd286a7ac7825233b930140be71cbfe3b5892
ArtfulAlpha <artfulalpha@gmail.com>
2021-11-14 03:13:22 +0200

    Refactoring 2 version

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const DONE_STATUS = "Done";
+const PROGRESS_STATUS = "progress";
+const TO_DO_STATUS = "ToDo";
+const TASK_PRIORITI_LOW = "low";
+const TASK_PRIORITI_HIGH = "high";
+
 const list = [
   {
     id: 1,
@@ -23,11 +29,15 @@ function findTask(taskname) {
   return (taskIndex = list.findIndex((elem) => elem.taskName == taskname));
 }
 
-function changeStatus(taskname, statusTask = "ToDo") {
+function changeStatus(taskname, statusTask = TO_DO_STATUS) {
   list[findTask(taskname)].taskStatus = statusTask;
 }
 
-function addTask(taskname, status = "ToDo", prioriti = "low") {
+function addTask(
+  taskname,
+  status = TO_DO_STATUS,
+  prioriti = TASK_PRIORITI_LOW
+) {
   if (list.find((elem) => elem.taskName == taskname)) {
     return "This task already exists, try another";
   } else {
@@ -45,22 +55,22 @@ function deleteTask(taskname) {
   // list.filter((elem) => elem.taskName !== taskname); Почему не работает такая конструкция + нельзя переназначить list поскольку это CONST
 }
 function showList() {
-  let resTodo = `Todo:\n`;
-  let resProg = "In Progress:\n";
-  let resDone = "Done:\n";
+  let resTodo = `${TO_DO_STATUS}:\n`;
+  let resProg = `In ${PROGRESS_STATUS}:\n`;
+  let resDone = `${DONE_STATUS}:\n`;
   let out = "";
   for (let i = 0; i < list.length; i++) {
-    if (list[i].taskStatus == "ToDo") {
+    if (list[i].taskStatus == TO_DO_STATUS) {
       resTodo += `${list[i].taskName} - prioriti: ${list[i].taskPrioriti}\n`;
     }
   }
   for (let i = 0; i < list.length; i++) {
-    if (list[i].taskStatus == "progress") {
+    if (list[i].taskStatus == PROGRESS_STATUS) {
       resProg += `${list[i].taskName} - prioriti: ${list[i].taskPrioriti}\n`;
     }
   }
   for (let i = 0; i < list.length; i++) {
-    if (list[i].taskStatus == "Done") {
+    if (list[i].taskStatus == DONE_STATUS) {
       resDone += `${list[i].taskName} - prioriti: ${list[i].taskPrioriti}\n`;
     }
   }
